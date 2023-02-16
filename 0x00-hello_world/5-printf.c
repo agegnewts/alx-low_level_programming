0c55572b5a72adcc2b4eeb4e6458b217a9e61d29
agegnewts <agegnew.tsega@gmail.com>
2023-02-16 18:40:45 +0300

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-/*
-* main - Entry point
-* return: return 0 meaning (success)
-*
-*/
+/**
+ *  main - Entry point
+ *
+ *  character string called name is called to hold the string for printing
+ *
+ *  return will return 0 for proper exectuion
+ *
+ *  Return: Always 0 (success)
+ */
+
 int main(void)
 {
 
