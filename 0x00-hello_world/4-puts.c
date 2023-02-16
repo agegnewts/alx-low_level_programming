7b4c28fcf83dd3250c4b23b19d16c087a0bac9c8
agegnewts <agegnew.tsega@gmail.com>
2023-02-16 16:44:07 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** 
+ *  main - Entry point
+ *
+ *  character string called name is called to hold the string for printing
+ *
+ *  puts function will take the string argument name to print it
+ *
+ *  return will return 0 for proper exectuion
+ */
+int main (void)
+{
+	char name[]=" \"Programming is like building a multilingual puzzle";
+	puts(name);
+	return 0;
+}
