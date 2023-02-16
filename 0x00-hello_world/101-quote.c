cd0a1f82c4eade3dbcebf2edbf2d643fb47f745f
agegnewts <agegnew.tsega@gmail.com>
2023-02-16 20:12:01 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  character string called name is called to hold the string for printing
+ *
+ *  return will return 0 for proper exectuion
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (0);
+
+}
