e7507cf4803a79cc0f29b572046361ee150af57d
agegnewts <agegnew.tsega@gmail.com>
2023-02-21 22:12:03 +0300

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = isalpha('H');
+	_putchar(r + '0');
+	r = isalpha('o');
+	_putchar(r + '0');
+	r = isalpha(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
