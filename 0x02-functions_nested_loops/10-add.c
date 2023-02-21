ae55474db76fcae20af764e1faae1e8a22b333b1
agegnewts <agegnew.tsega@gmail.com>
2023-02-21 23:19:48 +0300

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * add - add two numbers
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * Return: add of two numbers
+ */
+
+int add(int a, int b)
+{
+	int add;
+
+	add = a + b;
+	return (add);
+}
