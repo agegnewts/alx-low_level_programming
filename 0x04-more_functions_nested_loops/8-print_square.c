93dc93794f104aa4898ac49ee8f9970d0b3754d6
agegnewts <agegnew.tsega@gmail.com>
2023-02-24 03:03:46 +0300

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square n times
+ * @size: number of squares
+ * Return: empty
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+				for (j = 0; j < size; j++)
+			{
+				putchar(35);
+			}
+		putchar('\n');
+		}
+	}
+}
