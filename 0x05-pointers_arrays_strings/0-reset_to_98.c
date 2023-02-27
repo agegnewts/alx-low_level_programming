171b67a8dc6791e334fb1713b34eab4acf35d47c
agegnewts <agegnew.tsega@gmail.com>
2023-02-27 13:52:30 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets the value of n to 98
+ * @n: returns new value of n 98
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
