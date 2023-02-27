9db9c806bb645ccfcf522a238b78a4598dad4db6
agegnewts <agegnew.tsega@gmail.com>
2023-02-27 16:11:28 +0300

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int inicount = 0;
+	int secondcount;
+
+	while (*s != '\0')
+	{
+		inicount++;
+		s++;
+	}
+	/* s--;w */
+	for (secondcount = inicount; secondcount > 0;  secondcount--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
