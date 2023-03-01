ac1c68b5c026ca7a4ac827b58c6dc39e5c9f7423
agegnewts <agegnew.tsega@gmail.com>
2023-03-02 00:07:17 +0300

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy -  returns the copied string
+ * @dest: string that gets a copy
+ * @src: string that will be copied
+ * @n:  the number of strings that will be copied
+*/
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
