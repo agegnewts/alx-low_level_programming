a679c8bea732a30c9862287efd933a6e03f57671
agegnewts <agegnew.tsega@gmail.com>
2023-03-08 13:04:18 +0300

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  * @x: value to raise
  * @y: power
  *
- * Return: 
+ * Return:
  */
 int _pow_recursion(int x, int y)
 {
@@ -14,7 +14,7 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 	if (y < 0)
-	{	
+	{
 		return (-1);
 	}
 	return (x * _pow_recursion(x, y - 1));
