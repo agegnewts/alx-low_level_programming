61634d37ee4742009c2b312839b71cfd45813343
agegnewts <agegnew.tsega@gmail.com>
2023-03-08 11:42:42 +0300

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse
+ * @s: pointer to string params
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')  
+	{
+
+		_putchar(*s);
+		_print-rev_recursion(s+1)
+
+	}
+}
