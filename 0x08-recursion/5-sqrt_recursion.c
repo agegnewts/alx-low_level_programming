1c93466c58b6e4385e5d61a8cc0a99448336619d
agegnewts <agegnew.tsega@gmail.com>
2023-03-08 14:10:25 +0300

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,9 @@
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
+ * _sqrt_recursion_real - a wrapper that does the recursion bit
  *
  * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
+ * @a: minimum number to guess
+ * @b: maximum number to guess
  *
  * Return: square root of @n or -1
  */
@@ -11,22 +11,22 @@ int _sqrt_recursion_real(int n, int a, int b)
 {
 	int numb, numb_sqd;
 
-	numb = (a + b) / 2;	
-	numb_sqd = numb * numb;	
+	numb = (a + b) / 2;
+	numb_sqd = numb * numb;
 
-	if (numb_sqd == n)		
+	if (numb_sqd == n)
 		return (numb_sqd);
-	else if (a == b)		
+	else if (a == b)
 		return (-1);
-	else if (numb_sqd < n)	
+	else if (numb_sqd < n)
 		return (_sqrt_recursion_real(n, numb + 1, b));
-	else				
+	else
 		return (_sqrt_recursion_real(n, a, numb - 1));
 }
 
 /**
  * _sqrt_recursion - a function that returns the natural
- *                   
+ *
  *
  * @n: input number
  *
@@ -34,11 +34,11 @@ int _sqrt_recursion_real(int n, int a, int b)
 */
 int _sqrt_recursion(int n)
 {
-	if (n == 1)		
+	if (n == 1)
 		return (1);
-	else if (n == 0)	
+	else if (n == 0)
 		return (0);
-	else if (n < 0)		
+	else if (n < 0)
 		return (-1);
 	else
 		return (_sqrt_recursion_real(n, 1, n));
